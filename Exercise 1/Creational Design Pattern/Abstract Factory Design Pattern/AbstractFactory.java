06ca8a287503adc53dce61beeb8f3c9c7d949663
Abdul Baasith <152968647+AbdulBaasith005@users.noreply.github.com>
2025-09-30 00:05:11 +0530

    Update AbstractFactory.java

--- Exercise 1/Creational Design Pattern/Abstract Factory Design Pattern/AbstractFactory.java
+++ Exercise 1/Creational Design Pattern/Abstract Factory Design Pattern/AbstractFactory.java
@@ -1,5 +1,4 @@
 import src.*;
-
 import java.util.Scanner;
 
 public class AbstractFactory {
@@ -7,20 +6,24 @@ public class AbstractFactory {
         Scanner sc  = new Scanner(System.in);
         System.out.print("1. Google\n2. Alexa\nEnter Choice: ");
         int choice = sc.nextInt();
+        
         SmartHomeFactory factory = null;
         switch (choice) {
             case 1 -> factory = new GoogleFactory();
             case 2 -> factory = new AlexaFactory();
         }
+        
         if (factory == null) return;
         Speaker speaker = factory.createSpeaker();
         Light light = factory.createLight();
         speaker.playSound();
         light.turnOn(speaker.getBrand());
         sc.nextLine();
+        
         System.out.print("Turn OFF light? (Yes/No): ");
         String str = sc.nextLine();
         if (str.equalsIgnoreCase("yes")) light.turnOff(speaker.getBrand());
         else System.exit(0);
     }
+
 }
