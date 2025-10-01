323ba1352785b4de9ecb08be4d98dcb34c340af7
Abdul Baasith <152968647+AbdulBaasith005@users.noreply.github.com>
2025-10-02 05:21:31 +0530

    Update RoomNotBookedException.java

--- Exercise 2/office/exception/RoomNotBookedException.java
+++ Exercise 2/office/exception/RoomNotBookedException.java
@@ -2,6 +2,7 @@ package office.exception;
 
 public class RoomNotBookedException extends RuntimeException {
     public RoomNotBookedException(int roomId) {
-        super("Room " + roomId + " is not booked. Occupants cannot be added.");
+        super("Room " + roomId + " is not booked. Cannot cancel booking or cannot add occupants.");
     }
 }
+
